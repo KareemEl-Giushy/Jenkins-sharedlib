32613a58db149dd81474574da793bfe1aaa81d9f
Kareem El-Giushy <kareem.64123@gmail.com>
2026-05-26 06:54:23 +0300

    Follow the security standard

--- src/com/iti/DockerClass.groovy
+++ src/com/iti/DockerClass.groovy
@@ -9,5 +9,5 @@ def pushImage(String imageName, String imageTag) {
 }
 
 def dockerLogin(String username, String password) {
-    sh "echo ${password} | docker login -u ${username} --password-stdin"
+    sh 'echo ${password} | docker login -u ${username} --password-stdin'
 }
