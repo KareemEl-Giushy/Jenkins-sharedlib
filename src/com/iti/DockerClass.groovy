a0f60f3d7b79f1da6c5e83d892f05d75fa73b02a
Kareem El-Giushy <kareem.64123@gmail.com>
2026-05-26 07:05:12 +0300

    fix a fucken typo

--- src/com/iti/DockerClass.groovy
+++ src/com/iti/DockerClass.groovy
@@ -9,7 +9,7 @@ def pushImage(String imageName, String imageTag) {
 }
 
 def dockerLogin(String credientialId) {
-    withCredentials([usernamePassword(usernameVariable="username", passwordVariable="password", credentialsId=credientialId)]) {
+    withCredentials([usernamePassword(usernameVariable: "username", passwordVariable: "password", credentialsId: credientialId)]) {
         sh 'echo ${password} | docker login -u ${username} --password-stdin'
     }
 }
