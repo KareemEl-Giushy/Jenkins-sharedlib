fd8eceaafcd71480d798b510061270e70bba5d91
Kareem El-Giushy <kareem.64123@gmail.com>
2026-05-26 06:50:23 +0300

    Remove Class to keep context as is in the pipeline

--- src/com/iti/DockerClass.groovy
+++ src/com/iti/DockerClass.groovy
@@ -1,15 +1,13 @@
 package com.iti;
 
-class DockerClass {
-    def buildImage(String imageName, String imageTag) {
-        sh "docker build -t ${imageName}:${imageTag} ."
-    }
+def buildImage(String imageName, String imageTag) {
+    sh "docker build -t ${imageName}:${imageTag} ."
+}
 
-    def pushImage(String imageName, String imageTag) {
-        sh "docker push ${imageName}:${imageTag}"
-    }
+def pushImage(String imageName, String imageTag) {
+    sh "docker push ${imageName}:${imageTag}"
+}
 
-    def dockerLogin(String username, String password) {
-        sh "echo ${password} | docker login -u ${username} --password-stdin"
-    }
+def dockerLogin(String username, String password) {
+    sh "echo ${password} | docker login -u ${username} --password-stdin"
 }
--- src/com/iti/MVNClass.groovy
+++ src/com/iti/MVNClass.groovy
@@ -1,11 +1,10 @@
 package com.iti;
 
-class MVNClass {
-    def buildProject(comandOption) {
-        sh "mvn ${comandOption}"
-    }
 
-    def runTests() {
-        sh "mvn test"
-    }
+def buildProject(comandOption) {
+    sh "mvn ${comandOption}"
+}
+
+def runTests() {
+    sh "mvn test"
 }
